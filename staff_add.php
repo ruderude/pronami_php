205c7c405b70acd7a3df29c0773277d9563b5f7a
ruderude <rude1979@gmail.com>
2021-04-15 13:07:55 +0900

    First commit

---
+++ staff_add.php
@@ -0,0 +1,28 @@
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>ろくまる農園</title>
+</head>
+
+<body>
+    スタッフ追加<br>
+    <br>
+    <form method="post" action="staff_add_check.php">
+        スタッフ名を入力してください。<br>
+        <input type="text" name="name" value="" style="width:200px"><br>
+        パスワードを入力してください。 <br>
+        <input type="password" name="pass1" style="width:100px"><br>
+        パスワードをもう一度入力してください。 <br>
+        <input type="password" name="pass2" style="width:100px"><br>
+        <br>
+        <input type="button" onclick="history.back()" value="戻る"><br>
+        <input type="submit" value="OK"><br>
+    </form>
+</body>
+
+</html>
---
+++ staff_add_check.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>チェック</title>
+</head>
+<body>
+    <?php
+    // var_dump($_POST);
+    $staff_name = $_POST["name"];
+    $staff_pass1 = $_POST["pass1"];
+    $staff_pass2 = $_POST["pass2"];
+
+    $staff_name = htmlspecialchars($staff_name, ENT_QUOTES, 'UTF-8');
+    $staff_pass1 = htmlspecialchars($staff_pass1, ENT_QUOTES, 'UTF-8');
+    $staff_pass2 = htmlspecialchars($staff_pass2, ENT_QUOTES, 'UTF-8');
+
+    if($staff_name == '') {
+        print 'スタッフ名が入力されていません。<br />';
+    } else {
+        print 'スタッフ名：';
+        print $staff_name;
+        print '<br />';
+    }
+
+    if($staff_pass1 == '' || $staff_pass2 == ''){
+        print 'パスワードが空です。<br />';
+    }
+
+    if($staff_pass1 != $staff_pass2) {
+        print 'パスワードが一致しません。<br />';
+    }
+
+    if ($staff_name == '' || $staff_pass1 == '' || $staff_pass1 != $staff_pass2) {
+        print
+        '<form>';
+        print
+        '<input type="button" onclick="history.back()" value="戻る">';
+        print '</form>';
+    } else {
+        $staff_pass1 = md5($staff_pass1);
+        print
+        '条件OKです。';
+        print
+        '<form method="post" action="staff_add_check.php">';
+        print
+        '<input type="hidden" name="name" value="' . $staff_name . '">';
+        print
+        '<input type="hidden" name="pass" value="' . $staff_pass1 . '">';
+        print '<br />';
+        print '<input type="button" onclick="history.back()" value="戻る">';
+        print '<input type="submit" value="OK">';
+    }
+    ?>
+</body>
+</html>
